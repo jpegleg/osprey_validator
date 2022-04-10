3a9e80060f56ee8400c9f686cbb78649d8949929
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-04-09 23:34:40 -0600

    Update verify.sh

--- docker/osprey_1/verify.sh
+++ docker/osprey_1/verify.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-uid=100_$RANDOM$RANDOM
-echo -n $1 | xxd -r -p > /var/tmp/$uid
+uid="/var/tmp/100_$RANDOM$RANDOM"
+echo -n $1 | xxd -r -p > $uid
 openssl rsautl -verify -in /var/tmp/$uid -inkey /opt/jwt/sign.pub -pubin
+rm -f $uid
