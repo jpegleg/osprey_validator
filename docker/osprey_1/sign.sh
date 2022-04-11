217a03a86651966ee3ff9d4ec9480306d5219fc7
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-04-10 21:35:13 -0600

    Update sign.sh

--- docker/osprey_1/sign.sh
+++ docker/osprey_1/sign.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-uid="/var/tmp//99_$RANDOM$RANDOM"
+uid="/var/tmp/99_$RANDOM$RANDOM"
 echo -n $1 > $uid
 openssl rsautl -sign -in /var/tmp/$uid -inkey /opt/jwt/sign.key | xxd -p | tr -d '\n'
 rm -rf $uid
